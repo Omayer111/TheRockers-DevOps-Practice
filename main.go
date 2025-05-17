d9b67be90a2fd1ce3c4f5d9834d6d0a297167039
Abu Omayer <abuomayer.111@gmail.com>
2025-05-17 16:31:20 +0600

    Dockerfile created and image created with base and then using distroless to reducing the size and enhancing security of the image

--- main.go
+++ main.go
@@ -6,31 +6,34 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
 	http.ServeFile(w, r, "static/home.html")
 }
 
-func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
-	http.ServeFile(w, r, "static/courses.html")
-}
-
 func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
 	http.ServeFile(w, r, "static/about.html")
 }
 
+func schedulePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/schedule.html")
+}
+
 func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
 	http.ServeFile(w, r, "static/contact.html")
 }
 
 func main() {
-
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Redirect root to /home
+		http.Redirect(w, r, "/home", http.StatusFound)
+	})
 	http.HandleFunc("/home", homePage)
-	http.HandleFunc("/courses", coursePage)
 	http.HandleFunc("/about", aboutPage)
+	http.HandleFunc("/schedule", schedulePage)
 	http.HandleFunc("/contact", contactPage)
+
+	// Serve static assets (CSS, images)
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
